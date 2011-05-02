f1df89f64245e360f60ef0dd1ab95a6f4d95fb9a
Alan Palazzolo <github@zzolo.org>
2011-05-01 17:17:37 -0700

    Added data and switched maps back to OL and wax.

--- site/js/maps.js
+++ site/js/maps.js
@@ -9,51 +9,86 @@ document.namespaces;
  */
 (function($) {
   $(document).ready(function() {
-    var mm = com.modestmaps;
     
-    // Primary map
-    var mPrimary = new mm.Map(
-      'map-primary',
-      new com.modestmaps.WaxProvider({
-        baseUrl: 'http://a.tiles.mapbox.com/mapbox/',
-        layerName: 'world-light'
-      })
-    );
-    mPrimary.setCenterZoom(new com.modestmaps.Location(38.8225909, -97.5585), 4);
-    
-    // Secondary map
-    var mSecondary = new mm.Map(
-      'map-secondary',
-      new com.modestmaps.WaxProvider({
-        baseUrl: 'http://a.tiles.mapbox.com/mapbox/',
-        layerName: 'world-light'
-      })
-    );
-    mSecondary.setCenterZoom(new com.modestmaps.Location(38.8225909, -97.5585), 5);
-    
-    
-    
-/* OpenLayes maps
     var options = {};
     
     // Set Custom image path (TODO make local)
     OpenLayers.ImgPath = 'http://js.mapbox.com/theme/dark/';
     
-    // Set options for map
-    options.projection = new OpenLayers.Projection('EPSG:900913');
-    options.displayProjection = new OpenLayers.Projection('EPSG:4326');
-    options.maxExtent = new OpenLayers.Bounds(
-        -20037508.34, -20037508.34, 20037508.34, 20037508.34);
-    }
-    options.maxResolution = 1.40625;
-    options.controls = [];
+    var waxPrimary = { 'wax': [
+      '@group',
+      [
+        '@new OpenLayers.Map',
+        'map-primary',
+        {
+          'maxExtent': [
+            '@new OpenLayers.Bounds', -20037508.34, -20037508.34, 20037508.34, 20037508.34
+          ],
+          "theme": "http://mapbox-js.s3.amazonaws.com/ol/2.8/mapbox.css",
+          "maxResolution": 1.40625,
+          "projection": [
+            '@new OpenLayers.Projection',
+            'EPSG:900913'
+          ],
+          "displayProjection": [
+            '@new OpenLayers.Projection',
+            'EPSG:900913'
+          ],
+          "units": "m"
+        }
+      ],
+      ['@inject addLayer',
+        ['@new OpenLayers.Layer.TMS',
+          'MapBox Layer', 
+          'http://a.tile.mapbox.com/',
+          {
+            "layername": 'world-light',
+            "type": 'png',
+            "projection": [
+              "@new OpenLayers.Projection",
+              "EPSG:900913"
+            ],
+            "serverResolutions": [
+              156543.0339,78271.51695,39135.758475,19567.8792375,9783.93961875,
+              4891.96980938,2445.98490469,1222.99245234,611.496226172,
+              305.748113086,152.874056543,76.4370282715,38.2185141357,
+              19.1092570679,9.55462853394,4.77731426697,2.38865713348,
+              1.19432856674,0.597164283371
+            ],
+            "resolutions": [
+              156543.0339,78271.51695,39135.758475,19567.8792375,9783.93961875,
+              4891.96980938,2445.98490469,1222.99245234,611.496226172,
+              305.748113086,152.874056543,76.4370282715,38.2185141357,
+              19.1092570679,9.55462853394,4.77731426697,2.38865713348,
+              1.19432856674,0.597164283371
+            ],
+            'maxExtent': [
+              '@new OpenLayers.Bounds',
+              -20037508.34, -20037508.34, 20037508.34, 20037508.34
+            ]
+          }
+        ]
+      ],
+      ['@inject zoomTo', 3]
+    ]};
+
+    // Get some nice controls
+    OpenLayers.ImgPath = "http://js.mapbox.com/theme/dark/";
     
-    // Make map
-    var mainMap = new OpenLayers.Map(mapID, options);
+    // Create maps
+    var mapPrimary = wax.Record(waxPrimary.wax);
+    var waxSecondary = waxPrimary;
+    waxSecondary.wax[1][1] = 'map-secondary';
+    var mapSecondary = wax.Record(waxSecondary.wax);
     
-    // Add layers
-    mainMap.addLayers();
-*/
-  
+    // Data stuff
+    $.getJSON('data/test/vaccination-total.json', function(data) {
+      var items = [];
+      $.each(data, function(key, val) {
+        // var geocodeURL = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + val.Area + '&sensor=false';
+        //$.getJSON(geocodeURL, function(data) {});
+      });
+    });
+    
   });
 })(jQuery);
