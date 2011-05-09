2080c5a2918463264348be4d129ac89258a4b62f
Alan Palazzolo <github@zzolo.org>
2011-05-08 22:17:48 -0700

    Add ability to have authentication variables in non-versioned file.

---
+++ data-science/authentication/auth.example.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * This file is meant to hold authenitcation information.
+ *
+ * This is the example file.  The real file should be auth.php
+ * in the same directory.
+ */
+$simplegeo_oath_token = '';
+$simplegeo_oath_secret = '';
