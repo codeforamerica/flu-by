6803708796a8604b805039a9c03b3a943a16a863
Alan Palazzolo <github@zzolo.org>
2011-05-08 23:57:43 -0700

    Added SimpleGeo context querying to get ploygon of states.

--- data-science/README.md
+++ data-science/README.md
@@ -5,3 +5,25 @@ This directory is meant for scripts that have manipulated the data.
 1. [Sag](http://www.saggingcouch.com/)
 2. [GeoPHP](https://github.com/phayes/geoPHP)
 3. [Krumo](http://krumo.sourceforge.net/)
+4. [SimpleGeo PHP PEAR Library]()
+5. Google Geocoding Services (some inspiration from [here](http://eileenaw.wordpress.com/2010/07/18/google-map-geocoding-using-php-codes-for-wordpress/))
+
+## PEAR on Mac
+
+For some geocoding services from SimpleGeo and PHP, it is easier
+to get PEAR installed.  Here are some basics steps for Mac, which
+assume you just want it for your user and are using MAMP.  Also 
+note that MAMP comes with a version of PEAR, and that if you want
+to use that one, the first couple steps are not needed:
+
+1. `wget http://pear.php.net/go-pear.phar`
+2. `php -d detect_unicode=0 go-pear.phar`
+    * You could change where it is installed here
+3. I would suggest aliasing the pear path in your .bash_profile
+   * `alias pear='/path/to/pear/bin/pear'`
+4. Check it is working: `pear config-show`
+   * You should get a list of configs
+5. Update php.ini in MAMP to include new directory.  This will probably be here:
+   * `/Applications/MAMP/conf/php5.3/php.ini`
+   * Add the following to the list of included paths: `include_path = "<WHAT IS ALREADY THERE>:/path/to/pear/share/pear"`
+       
--- data-science/convert-vacc-to-geo.php
+++ data-science/convert-vacc-to-geo.php
@@ -5,12 +5,17 @@
  *
  * We are using Sag to connect to Couch DB.
  */
+session_start();
 
 // Includes
 require_once 'libraries/krumo/class.krumo.php';
 require_once 'libraries/sag-0.4.0/src/Sag.php';
 require_once 'libraries/geoPHP/geoPHP.inc';
 
+// SimpleGeo
+set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . '/libraries/');
+require_once 'Services_SimpleGeo/Services/SimpleGeo.php';
+require_once 'authentication/auth.php';
 
 // Variables
 $server = 'health.iriscouch.com';
@@ -18,11 +23,19 @@ $port = '5984';
 $db_orig = 'vaccination';
 $db_new = 'flu-features';
 $records = array();
-$geocoded = array();
 
 // Connect to database
 $sag = new Sag($server, $port);
 $sag->setDatabase($db_orig);
+
+// Create SimpleGeo client
+$simple_geo = new Services_SimpleGeo($simplegeo_oath_token, $simplegeo_oath_secret);
+
+// Set up session for geocoding, since we don't need to do
+// it more than once, usually
+if (empty($_SESSION['geocoded']) || !empty($_GET['geocode_reset'])) {
+  $_SESSION['geocoded'] = array();
+}
 
 // Get some records
 try {
@@ -33,7 +46,7 @@ try {
   foreach ($all->body->rows as $key => $doc_meta) {
     $count++;
     // Limit for testing
-    if ($count > 10) continue;
+    if ($count > 50) continue;
     
     // Create a structure that will be helpful
     $doc = $sag->get('/' . $doc_meta->id);
@@ -47,15 +60,32 @@ try {
         );
         
     // Geocode, if we have not already
-    if (empty($geocoded[$area])) {
-      // Get state polygon
+    if (empty($_SESSION['geocoded'][$area])) {
+      $geometry = array();
+    
+      // Geocode
+      $location = geocode($area);
+      if (!empty($location)) {
+        // Save point
+        $_SESSION['geocoded'][$area]['place']['centroid']['lat'] = $location->lat->__toString();
+        $_SESSION['geocoded'][$area]['place']['centroid']['lon'] = $location->lng->__toString();
+      
+        // Get context
+        $geo_data = $simple_geo->getContext($location->lat->__toString(), $location->lng->__toString());
+
+        // Find the state feature
+        foreach ($geo_data->features as $k => $geo_feature) {
+          if ($geo_feature->classifiers[0]->subcategory == 'State') {
+            $geometry = $simple_geo->getFeature($geo_data->features[$k]->handle);
+          }
+        }
+      }
       
       // Mark and put in structure
-      $geocoded[$area] = TRUE;
-      $records[$area]['place'] = TRUE;
+      $_SESSION['geocoded'][$area]['place']['state_geometry'] = $geometry;
     }
+    $records[$area]['place'] = $_SESSION['geocoded'][$area]['place'];
   }
-  
   
   create_output($records, TRUE);
 } 
@@ -68,6 +98,12 @@ catch (SagException $e) {
   // Just debug to screen
   create_output($e, TRUE);
   create_output($e->getMessage(), TRUE);
+}
+catch (Services_SimpleGeo_Exception $e) {
+  // Just debug to screen
+  create_output($e, TRUE);
+  create_output($e->getMessage(), TRUE);
+  create_output($e->getCode(), TRUE);
 }
 
 // Make an HTML page or CLI output, accordingly
@@ -126,4 +162,36 @@ function print_page() {
 </body>
 </html>
   ';
+}
+
+/**
+ * Geocode a string using Googles Geocoder
+ */
+function geocode($input) {
+  // "OK" indicates that no errors occurred; the address 
+  //      was successfully parsed and at least one geocode was returned.
+  // "ZERO_RESULTS" indicates that the geocode was 
+  //      successful but returned no results. This may occur 
+  //      if the geocode was passed a non-existent address or 
+  //      a latlng in a remote location.
+  // "ZERO_RESULTS" indicates that you are over your quota.
+  // "REQUEST_DENIED" indicates that your request was denied, 
+  //     generally because of lack of a sensor parameter.
+  // "INVALID_REQUEST" generally indicates that the
+  //     query (address or latlng) is missing.
+  $delay = 100000;
+  set_time_limit(1000);
+  $base_url = "http://maps.google.com/maps/api/geocode/xml?sensor=false";
+  
+  // Make address.  Hard code US in there
+  $request_url = $base_url . "&address=" . urlencode($input).",USA";
+  $response = simplexml_load_file($request_url);
+  if ($response->status = 'OK') {
+		$location = $response->result->geometry->location;
+		return $location;
+	}
+	else {
+		create_output($response, TRUE);
+		return FALSE;
+	}
 }
